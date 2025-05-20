32d0a2742d614c21f5600599c8f1fb21846c2346
Poshangg <165940225+Poshangg@users.noreply.github.com>
2025-05-20 16:06:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1632,4 +1632,5 @@ if __name__ == '__main__':
 
     # Ensure you have run the 'flask init-db' command first. # Comment no longer accurate
     print("Starting Flask app...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
